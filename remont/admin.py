beb27f9c4cfc4782b39519b994e7a5d34fe0c290
siarheiNaralenkau <naralenkov2010@gmail.com>
2015-02-15 21:28:44 +0300

    1) Custom Admin was added for WorkType 2) jquery_ui and yandex_map_api libraries were added

--- remont/admin.py
+++ remont/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from remont.models import WorkCategory, WorkType, UserProfile, City, OrganizationProfile
 
 
+class WorkTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    search_fields = ('name',)
+    list_filter = ('category',)
+
 admin.site.register(WorkCategory)
-admin.site.register(WorkType)
+admin.site.register(WorkType, WorkTypeAdmin)
 admin.site.register(UserProfile)
 admin.site.register(City)
 admin.site.register(OrganizationProfile)
