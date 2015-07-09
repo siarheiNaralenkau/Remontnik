b4f367f7c0024b8fdecea2a65b987df22c42a8a4
siarheiNaralenkau <naralenkov2010@gmail.com>
2015-07-09 17:03:52 +0300

    1) Org profile mockup was added 2) Reference to collegues from organization profile was added.

--- remont/models.py
+++ remont/models.py
@@ -117,6 +117,8 @@ class OrganizationProfile(models.Model):
     password = models.CharField(u'Пароль', max_length=120, blank=True, null=True, default=None)
     login = models.CharField(u'Логин на сайте', max_length=100, blank=True, null=True, default=None)
 
+    collegues = models.ManyToManyField('self', verbose_name=u"Коллеги", related_name="collegues", null=True)
+
     def save(self, *args, **kwargs):
         if not self.landline_phone and not self.mobile_phone and not self.mobile_phone2 and not self.fax:
             raise ValidationError(u"Заполните хотя бы одно из полей: " + ", ".join([
