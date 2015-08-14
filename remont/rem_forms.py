0601a372c8048d1b20971bced26ebac8fdd7cca2
siarheiNaralenkau <naralenkov2010@gmail.com>
2015-08-14 12:35:43 +0300

    Example disabled field(name) for editting org profile was implemented

--- remont/rem_forms.py
+++ remont/rem_forms.py
@@ -25,7 +25,7 @@ class SuggestJobForm(forms.Form):
 
 class RegisterForm(forms.Form):
     def __init__(self, *args, **kwargs):
-        super(RegisterForm, self).__init__(*args, **kwargs)        
+        super(RegisterForm, self).__init__(*args, **kwargs)
 
     name = forms.CharField(max_length=100,
                            label=u'Название организации',
@@ -35,7 +35,7 @@ class RegisterForm(forms.Form):
     city = forms.ModelChoiceField(queryset=City.objects.order_by("name"), label=u"Город регистрации", required=False)
     address = forms.CharField(label="Адрес", max_length=180, required=False, widget=Textarea(attrs={'cols': 60, 'rows': 3}))
     description = forms.CharField(label=u"Обшая информация об организации", required=False, widget=forms.Textarea(attrs={"rows": 8, "cols": 60}))
-    
+
     landing_phone = forms.CharField(label=u"Стационарный телефон", max_length=30, required=False)
     mobile_phone = forms.CharField(label=u"Мобильный телефон", max_length=30, required=False)
     mobile_phone2 = forms.CharField(label=u"Второй мобильный телефон", max_length=30, required=False)
@@ -43,29 +43,29 @@ class RegisterForm(forms.Form):
     web_site = forms.URLField(label=u"Домашняя страница", max_length=100, required=False)
     email = forms.EmailField(label=u"Электронная почта", max_length=100, required=True, error_messages={"required": u"Укажите электронную почту"})
 
-    login = forms.CharField(label=u"Логин на сайте", 
-                            max_length=100, 
-                            required=True, 
+    login = forms.CharField(label=u"Логин на сайте",
+                            max_length=100,
+                            required=True,
                             error_messages={"required": u"Укажите логин аккаунта организации"})
-    password = forms.CharField(label="Пароль", 
-                               max_length=16, 
-                               required=True, 
-                               widget=PasswordInput(), 
+    password = forms.CharField(label="Пароль",
+                               max_length=16,
+                               required=True,
+                               widget=PasswordInput(),
                                error_messages={"required": u"Укажите пароль аккаунта организации"})
-    password_repeat = forms.CharField(label="Повторите пароль", 
-                                      max_length=16, 
-                                      required=True, 
-                                      widget=PasswordInput(), 
+    password_repeat = forms.CharField(label="Повторите пароль",
+                                      max_length=16,
+                                      required=True,
+                                      widget=PasswordInput(),
                                       error_messages={"required": u"Введите пароль повторно"})
 
-    work_cities = forms.ModelMultipleChoiceField(label="Города, где работаете", 
-                                                 required=True, 
-                                                 queryset=City.objects.order_by("name"), 
+    work_cities = forms.ModelMultipleChoiceField(label="Города, где работаете",
+                                                 required=True,
+                                                 queryset=City.objects.order_by("name"),
                                                  widget=CheckboxSelectMultiple(),
                                                  error_messages={"required": u"Укажите города, в которых работаете"})
-    job_types = forms.ModelMultipleChoiceField(label="Виды выполняемых работ", 
-                                               required=True, 
-                                               queryset=WorkType.objects.all(), 
+    job_types = forms.ModelMultipleChoiceField(label="Виды выполняемых работ",
+                                               required=True,
+                                               queryset=WorkType.objects.all(),
                                                widget=CustomCheckBoxSelectMultiple(),
                                                error_messages={"required": u"Укажите виды выполняемых работ"})
 
@@ -79,13 +79,19 @@ class OrganizationProfileModelForm(forms.ModelForm):
             'job_types': CustomCheckBoxSelectMultiple,
             'address': Textarea(attrs={'cols': 60, 'rows': 3}),
             'city': Select(attrs={'style': 'width: 200px; float: none'}),
-            'description': Textarea(attrs={'cols': 100, 'rows': 10, 'style': 'overflow: auto; margin-left: 20px'}),            
+            'description': Textarea(attrs={'cols': 100, 'rows': 10, 'style': 'overflow: auto; margin-left: 20px'}),
             'logo': SingleImageInput,
             'collegues': CheckboxSelectMultiple
         }
 
 
 class OrganizationEditForm(forms.ModelForm):
+  def __init__(self, *args, **kwargs):
+    super(OrganizationEditForm, self).__init__(*args, **kwargs)
+    instance = getattr(self, 'instance', None)
+    if instance and instance.pk:
+      self.fields['name'].widget.attrs['readonly'] = True
+
   class Meta:
     model = OrganizationProfile
     fields = ('name', 'logo', 'city', 'address', 'description',  'landline_phone', 'mobile_phone',
@@ -112,7 +118,7 @@ class OrganizationEditForm(forms.ModelForm):
         'job_types': CustomCheckBoxSelectMultiple(),
         'address': Textarea(attrs={'cols': 60, 'rows': 3}),
         'city': Select(attrs={'style': 'width: 200px; float: none'}),
-        'description': Textarea(attrs={'cols': 60, 'rows': 8}),            
+        'description': Textarea(attrs={'cols': 60, 'rows': 8}),
         'logo': SingleImageInput(),
         'collegues': CheckboxSelectMultiple(),
         'work_cities': CheckboxSelectMultiple()
@@ -123,4 +129,3 @@ class UploadPhotoForm(forms.ModelForm):
   class Meta:
     model = WorkPhoto
     fields = ('organization', 'album', 'photo')
-  
