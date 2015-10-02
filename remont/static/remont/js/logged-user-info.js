93567f0fce8e332cbd1d918c22e54968501f3cc9
siarheiNaralenkau <naralenkov2010@gmail.com>
2015-10-02 15:42:54 +0300

    Messages dialog - first iteration of implementation is finished

--- remont/static/remont/js/logged-user-info.js
+++ remont/static/remont/js/logged-user-info.js
@@ -4,8 +4,8 @@ $(function() {
     resizable: false,
     modal: true,
     title: "Сообщения",
-    height: 400,
-    width: 500
+    height: 480,
+    width: 640
   });
 
   function getNewMessages() {
@@ -14,7 +14,22 @@ $(function() {
 
   function fetchMessages(responseData) {
     console.log("Messages data: ", responseData);
+    var list = $("#messagesList");
+    list.html("");
+    for(var i = 0; i < responseData.length; i++) {
+      var msgData = responseData[i];
+      var msgTemplate = $("#personMsgTemplate").html();
+      var msgItem = "<li class='message-el' dataPersonId='" + msgData.sender_id + "'>" +
+        msgTemplate.format(msgData.from_logo, msgData.from_name, msgData.msg_text, msgData.msg_written, msgData.messages_count) + "</li>";
+      list.html(list.html() + msgItem);
+    }
+    $(".message-el").on("click", showConversationWithPerson);
     showMessagesDialog();
+  }
+
+  function showConversationWithPerson() {
+    var senderId = $(this).attr("dataPersonId");
+    alert("Showing conversation with person which id is: " + senderId + " should be implemented here");
   }
 
   function showMessagesDialog() {
--- remont/views.py
+++ remont/views.py
@@ -25,6 +25,8 @@ from django.forms.formsets import formset_factory
 
 from  django.contrib.auth.hashers import check_password
 
+from datetime import datetime, date, time
+
 # Главная страница приложения
 def index(request):
   top10 = {'top10 masters': 'top10 masters should be displayed here'}
@@ -672,16 +674,43 @@ def top_orgs(request):
 
 # Получаем новые сообщения для пользователя.
 def get_new_messages_for_user(request):
-  new_messages = Message.objects.filter(was_read__isnull=True, msg_to=request.user)
-  new_messages_result = []
+  new_messages = Message.objects.filter(was_read__isnull=True, msg_to=request.user).order_by("-was_written")
+  new_messages_result = {}
+  cur_datetime = datetime.now()
+  cur_date = cur_datetime.strftime("%d-%m-%Y")
+  cur_time = cur_datetime.strftime("%H:%M")
+
   for msg in new_messages:
     sender = msg.msg_from
-    sender_name = sender.username
-    sender_org = OrganizationProfile.objects.filter(account=sender).first()
-    if sender_org:
-      sender_logo = get_org_logo(sender_org)
-    else:
-      sender_logo = "/static/remont/images/info_empty.jpg"
-    new_messages_result.append({"from_name": sender_name, "from_logo": sender_logo, "msg_text": msg.text, "msg_written": msg.was_written})
+    sender_id = str(sender.id)
+    if not sender_id in new_messages_result:
+      sender_name = sender.username
+      sender_org = OrganizationProfile.objects.filter(account=sender).first()
+      if sender_org:
+        sender_logo = get_org_logo(sender_org)
+      else:
+        sender_logo = "/static/remont/images/info_empty.jpg"
+      was_written_date = msg.was_written.strftime("%d-%m-%Y")
+      was_written_time = msg.was_written.strftime("%H:%M")
+      if was_written_date == cur_date:
+        was_written = was_written_time
+      else:
+        was_written = was_written_date
 
-  return JsonResponse(new_messages_result, safe=False)
+      msg_item = {"from_name": sender_name, "from_logo": sender_logo, "msg_text": msg.text, "msg_written": was_written, "messages_count": 1}
+      new_messages_result[sender_id] = msg_item
+    else:
+      new_messages_result[sender_id]["messages_count"] = new_messages_result[sender_id]["messages_count"] + 1
+
+  new_messages_array = []
+  for key, data in new_messages_result.iteritems():
+    new_messages_array.append({
+      "sender_id": key,
+      "from_name": data["from_name"],
+      "from_logo": data["from_logo"],
+      "msg_text": data["msg_text"],
+      "msg_written": data["msg_written"],
+      "messages_count": data["messages_count"]
+    })
+
+  return JsonResponse(new_messages_array, safe=False)
