55638b229b7bc812cadfe14d522768b2d67aac6f
siarheiNaralenkau <naralenkov2010@gmail.com>
2016-01-29 11:21:41 +0300

    Host name issue is fixed

--- remontnik/settings.py
+++ remontnik/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '-b1o=+9@9#*zs$iq@b4%c^r1e#845!gcz_#@b4^*c#vyo8-fx7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if socket.get_hostname() == 'staatix.by':
+if socket.gethostname() == 'staatix.by':
   DEBUG = false
   TEMPLATE_DEBUG = false
 else:
